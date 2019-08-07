ee3ea04e3b61656228e1a0664702b9e3aae1eb1c
Zhou Peng <zhoupeng10@xin.com>
2019-08-07 15:24:07 +0800

    remove unused code

--- src/main/java/com/zzz/orderpool/service/impl/MultiOrderService.java
+++ src/main/java/com/zzz/orderpool/service/impl/MultiOrderService.java
@@ -10,9 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
--- src/main/java/com/zzz/orderpool/service/impl/SimpleOrderService.java
+++ src/main/java/com/zzz/orderpool/service/impl/SimpleOrderService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
